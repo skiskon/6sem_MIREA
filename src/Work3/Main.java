4d3ce49142754a7c5c4111fce5950eef3cac602d
Max <228>
2018-05-29 11:56:55 +0300

    Финальный вариант всех работ

--- src/Work3/Main.java
+++ src/Work3/Main.java
@@ -7,37 +7,51 @@ import java.util.SortedMap;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> one = new ArrayList<>();
-        one.add(2);
-        one.add(3);
-        one.add(5);
-        one.add(4);
+//        one.add(2);
+//        one.add(3);
+//        one.add(5);
+//        one.add(4);
         ArrayList<Integer> two = new ArrayList<>();
-        two.add(1);
-        two.add(2);
-        two.add(3);
-        two.add(4);
+//        two.add(1);
+//        two.add(2);
+//        two.add(3);
+//        two.add(4);
+
+        int rand;
+        int a = 8;
+        for (int i = 0;i < 15; i++){
+            rand = (int) ( 1+ (Math.random() * a));
+            one.add(rand);
+
+            rand = (int) ( 1+ (Math.random() * a));
+            two.add(rand);
+
+
+        }
         Dynamic dynamic = new Dynamic();
         long start1 = System.nanoTime();
         ArrayList<Integer> result = dynamic.BiggstSubSequence(one, two);
         long finish1 = System.nanoTime();
         System.out.println(finish1-start1);
         System.out.println(Arrays.toString(result.toArray()));
+        System.out.println(Arrays.toString(one.toArray()));
+        System.out.println(Arrays.toString(two.toArray()));
 
 
 
 
         FullSearch fullSearch = new FullSearch();
         long start2 = System.nanoTime();
-        long sf = System.currentTimeMillis();
+//        long sf = System.currentTimeMillis();
         result = fullSearch.BiggestSubSequence(one, two);
         long finish2 = System.nanoTime();
-        long fi = System.currentTimeMillis();
+//        long fi = System.currentTimeMillis();
         System.out.println(finish2-start2);
-        System.out.println(start2);
-        System.out.println(finish2);
-        System.out.println(sf-fi);
-        System.out.println(sf);
-        System.out.println(fi);
+  //      System.out.println(start2);
+ //       System.out.println(finish2);
+  //      System.out.println(sf-fi);
+ //       System.out.println(sf);
+//        System.out.println(fi);
         System.out.println(Arrays.toString(result.toArray()));
     }
 }
